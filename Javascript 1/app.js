25bacd6a47e51f3ea2035918d5fb7e8e5c12e22e
Christian <112215625+christianrocha383@users.noreply.github.com>
2023-03-12 17:06:38 -0300

    Add files via upload

---
+++ Javascript 1/app.js
@@ -0,0 +1,5 @@
+function map () {
+    const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+    const changeNumbers = randomNumbers.filter(numbers => numbers % 2)
+    console.log(changeNumbers)
+    }    
