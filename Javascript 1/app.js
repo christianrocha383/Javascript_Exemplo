56e1784d09b66f752deee1b675e566b786b7f8a6
Christian <112215625+christianrocha383@users.noreply.github.com>
2023-03-12 19:50:36 -0300

    Update app.js

--- Javascript 1/app.js
+++ Javascript 1/app.js
@@ -1,11 +1,11 @@
 /*
 
-Gerando um novo array só com números ímpares e exibindo no console.
+Gerando um novo array só com números ímpares.
 
 */
 
-function map () {
+
     const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
     const changeNumbers = randomNumbers.filter(numbers => numbers % 2)
     console.log(changeNumbers)
-    }    
+    
